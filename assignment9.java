119385931b7b053c79e9c45600503562a0507fcf
sleeplessaaa <chen.lix@husky.neu.edu>
2017-04-01 21:16:04 -0700

    Add files via upload

---
+++ assignment9.java
@@ -0,0 +1,107 @@
+1.	Which of the following is true?
+
+A.	A program will terminate only when all user threads stop running.
+B.	A program will terminate only when all daemon stop running.
+C.	A daemon thread always runs at Thread.MIN_PRIORITY.
+D.	None of the above.
+
+ANS:    A
+
+
+2.	Given the following Java code:
+What is the result?
+A.	A compile-time error is generated at line 6
+B.	A compile-time error is generated at line 7
+C.	A compile-time error is generated at line 8
+D.	A compile-time error is generated at line 9
+E.	None of the above
+
+ANS:    D
+
+
+
+
+3.	Given the following Java code: [5 points]
+ 
+ 
+
+Which of the following is true?
+A.	This program will go exception when compiling.
+B.	Obj1 runs at Thread.MIN_PRIORITY.
+C.	Obj2 runs at Thread.MIN_PRIORITY.
+D.	The Thread.setDaemon method can change Thread.MIN_PRIORITY.
+E.	None of the above.
+
+ANS:  B
+
+
+4.	Given the following Java code:
+ 
+ 
+
+Which of the following are possible results of attempting to compile and run the program?
+A.	prints: A
+B.	prints: 0
+C.	prints: null
+D.	prints: i
+E.	Compile©\time error
+
+ANS:    C
+
+
+5.	Given the following Java code:
+
+What is the result of attempting to compile and run the program?
+A.	The program compiles and runs fine but prints nothing.
+B.	prints: A
+C.	Compiler error
+D.	An IllegalThreadStateException is thrown at run©\time
+E.	None of the above
+ 
+
+ANS:     D
+
+
+
+
+6.	Given the following Java code:
+
+What is the result?
+A.	Compilation fails
+B.	An exception is thrown at runtime
+C.	The code executes and prints "running"
+D.	The code executes and prints "runningrunning"
+E.	The code executes and prints "runningrunningrunning"
+
+ANS:   E
+
+
+7.	Given the following Java code:
+ 
+ 
+
+Which of the following are possible results of attempting to compile and run the program?
+
+A.	ABAB
+B.	BABA
+C.	AABB
+D.	BBAA
+E.	ABBA
+
+ANS:   A
+
+
+
+
+8.	Given the following Java code:
+ 
+ 
+Which of the following are possible results of attempting to compile and run the program?
+
+A.	ABA
+B.	BAB
+C.	AAB
+D.	BBA
+E.	ABB
+
+ANS:   E
